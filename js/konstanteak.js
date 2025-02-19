3ed965b32a0fc11dee0053554f170e5d799bad29
pmartine082 <pmartinez082@ikasle.ehu.eus>
2025-02-19 15:31:34 +0100

    .

--- js/konstanteak.js
+++ js/konstanteak.js
@@ -1,6 +1,7 @@
 //export const API_URL = 'http://192.168.137.1:3000';
 export const MAC = '9A:BD:80:BE:EF:01';
-export const API_URL = 'http://localhost:3000';
+export const API_URL =   'https://330d-158-227-0-238.ngrok-free.app';
+//export const API_URL = 'http://localhost:3000';
 export class Ebaluazioa {
     constructor(idEbaluazioa, idEpaimahaikidea, idTaldea, idEzaugarria, puntuak, noiz) { 
         this.idEzaugarria = idEzaugarria;
