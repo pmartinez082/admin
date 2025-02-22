595c597272cf3899cc043f6d33e7e11697df0061
pmartine082 <pmartinez082@ikasle.ehu.eus>
2025-02-22 14:43:27 +0100

    ,

--- index.js
+++ index.js
@@ -7,24 +7,21 @@ import router from './app/router.js';
 const app = express();
 
 //* Configuration
-const host = '192.168.137.1';
+//const host = '192.168.137.1';
+const host = 'localhost';
 const port = 3000;
 
-//* Configure CORS properly with preflightContinue: true
+//* Configure CORS 
 app.use(cors({
-   // origin: 'https://pmartinez082.github.io/*', 
-   origin: 'https://private-network-access-permission-test.glitch.me',
+    origin: '*', 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
-    preflightContinue: true  // Importante para que nuestro middleware custom se ejecute después
 }));
 
 //* Custom Middleware para agregar los encabezados de red privada en todas las respuestas
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Private-Network', 'true');
-    res.setHeader('Private-Network-Access-Name', 'zerbitzaria');
-    res.setHeader('Private-Network-Access-ID', '9A:BD:80:BE:EF:01');
     if (req.method === 'OPTIONS') {
         res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         return res.sendStatus(204);
